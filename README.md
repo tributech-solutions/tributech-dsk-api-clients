0641545e348f0ed1f14c8a30900630803a761a00
Tobias Mühlberger <t.muehlberger@mailbox.org>
2020-03-04 11:42:06 +0100

    updated readme

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 A collection of API clients for easy access to the DataSpace Kit.
 
 ![Overview](overview.jpg)
+
+## Usage of .NET Core client
+
+Requires netstandard2.0 or higher.
+
+``` csharp
+var authHandler = new APIAuthHandler(tokenUrl, scope, clientId, clientSecret);
+var authorizedHttpClient = new HttpClient(authHandler))
+var apiClient = new DataAPIClient(nodeUrl, authorizedHttpClient);
+
+// Get values within the last 7 days
+ICollection<ReadValueDoubleModel> data = await apiClient.DoubleAllAsync(dataStreamId, DateTime.Now, DateTime.Now.AddDays(-7), "asc", null, null, null);
+```
--- examples/netcore/Program.cs
+++ examples/netcore/Program.cs
@@ -8,21 +8,28 @@ using Tributech.Dataspace.Clients;
 namespace Tributech.Dataspace.ClientExamples {
     class Program {
         // Api Client Config
-        private const string nodeUrl = "https://node.example.com";
-        private const string tokenUrl = "https://id.example.com";
-        private const string scope = "195d0b59-2113-4d67-ac6c-7116595e413b-full-access";
-        private const string clientId = "4c90a8bf-10de-4fe5-ae44-fcd4e9bdb602";
-        private const string clientSecret = "56af011e-7c22-48e7-a4be-fb867b0be504";
+        //private const string nodeUrl = "https://node.example.com";
+        //private const string tokenUrl = "https://id.example.com";
+        //private const string scope = "195d0b59-2113-4d67-ac6c-7116595e413b-full-access";
+        //private const string clientId = "4c90a8bf-10de-4fe5-ae44-fcd4e9bdb602";
+        //private const string clientSecret = "56af011e-7c22-48e7-a4be-fb867b0be504";
 
         private static readonly Guid dataStreamId = new Guid("0514974b-fe05-4264-8c63-a636c4bea6a0");
 
+
+
+        private const string nodeUrl = "https://data-api.development-tributech.dataspace.network";
+        private const string tokenUrl = "https://id.development-tributech.dataspace.network/connect/token";
+        private const string scope = "195d0b59-2113-4d67-ac6c-7116595e413b-full-access";
+        private const string clientId = "195d0b59-2113-4d67-ac6c-7116595e413b";
+        private const string clientSecret = "793da246-d86a-5b95-daf1-a16c507f062e";
         static async Task Main(string[] args) {
             var authHandler = new APIAuthHandler(tokenUrl, scope, clientId, clientSecret);
             using (var authorizedHttpClient = new HttpClient(authHandler)) {
                 var apiClient = new DataAPIClient(nodeUrl, authorizedHttpClient);
 
                 // Get data points within the last 7 days
-                ICollection<ReadValueDoubleModel> data = await apiClient.DoubleAllAsync(dataStreamId, DateTime.Now, DateTime.Now.AddDays(-17), "asc", null, null, null);
+                ICollection<ReadValueDoubleModel> data = await apiClient.DoubleAllAsync(dataStreamId, DateTime.Now, DateTime.Now.AddDays(-7), "asc", null, null, null);
 
                 foreach(var item in data) {
                     Console.WriteLine($"{item.Timestamp}: Value {item.Values.FirstOrDefault()}");
